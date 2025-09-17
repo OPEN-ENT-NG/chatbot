071a83eae0182c946dc5a3491aa75216160c3aa7
mariusestaque <marius.estaque@opendigitaleducation.com>
2025-09-17 14:26:15 +0200

    feat: #RBACK-117, zookeeper migration

--- src/main/java/fr/openent/chatbot/Chatbot.java
+++ src/main/java/fr/openent/chatbot/Chatbot.java
@@ -4,6 +4,8 @@ import fr.openent.chatbot.config.ChatbotConfig;
 import fr.openent.chatbot.controller.ChatbotController;
 import fr.openent.chatbot.controller.MonitoringController;
 import fr.openent.chatbot.service.ServiceFactory;
+import fr.wseduc.webutils.collections.SharedDataHelper;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import org.entcore.common.http.BaseServer;
 import org.entcore.common.storage.Storage;
@@ -13,17 +15,24 @@ public class Chatbot extends BaseServer {
 
 	@Override
 	public void start(Promise<Void> startPromise) throws Exception {
-		super.start(startPromise);
+		Promise<Void> promise = Promise.promise();
+		super.start(promise);
+		promise.future()
+				.compose(init -> StorageFactory.build(vertx, config))
+				.compose(storageFactory -> initChatbot(storageFactory))
+				.onComplete(startPromise);
+	}
+
+	private Future<Void> initChatbot(StorageFactory storageFactory) {
 		ChatbotConfig chatbotConfig = new ChatbotConfig(config);
 
-
-		Storage storage = new StorageFactory(vertx, config).getStorage();
+		Storage storage = storageFactory.getStorage();
 
 		ServiceFactory serviceFactory = new ServiceFactory(vertx, storage, config, chatbotConfig);
 
 		addController(new ChatbotController(serviceFactory));
 		addController(new MonitoringController(serviceFactory));
-		startPromise.tryComplete();
+		return Future.succeededFuture();
 	}
 
 }
